ea0456bf9fe1a3c3b10875c05c8224427e5941e9
Fity <imfity@gmail.com>
2014-12-08 20:17:15 +0800

    add leetcode Same Tree

---
+++ leetcode/SameTree/solution.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    # @param p, a tree node
+    # @param q, a tree node
+    # @return a boolean
+    def isSameTree(self, p, q):
+        if p is None or q is None:
+            return p == q
+        if p.val != q.val:
+            return False
+        if not self.isSameTree(p.left, q.left):
+            return False
+        if not self.isSameTree(p.right, q.right):
+            return False
+        return True
